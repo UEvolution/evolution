2a8aded5795869d26acd014af3262a5f9927ba34
dengxuchen <gogogodeng@163.com>
2017-05-25 16:18:13 +0800

    no message

--- app.js
+++ app.js
@@ -1,8 +1,55 @@
 var express = require('express')
 var app = express()
+var mysql = require('mysql')
+var bodyParser = require('body-parser')
+var multer = require('multer')
 var router = require('./router')
 var config = require('./config')
-var mysql = require('mysql')
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+//解析文件上传类型
+//app.use(multer());
+
+//加载静态目录
+app.use(express.static('public'))
+
+//设置cookie session
+app.use(cookieParser('sessiontest'));
+app.use(session({
+	secret: 'sessiontest', //与cookieParser中的一致
+	resave: true,
+	saveUninitialized: true
+}));
+
+
+app.post('/login', function (req, res) {
+	console.log(req.body);
+	var user = {
+		name: "Chen-xy",
+		age: "22",
+		address: "bj"
+	}
+	req.session.user = user
+	res.json(user)
+})
+
+app.get('/info', function (req, res) {
+	if (req.session.user) {
+		var user = req.session.user;
+		res.json({
+			user: user
+		})
+	} else {
+		res.json({
+			error: '未登录'
+		})
+	}
+})
 
 
 var connection = mysql.createConnection(config.db)
